146fcc208c34e03621a59cb341f45264354ab165
martonsereg <marton.sereg@sequenceiq.com>
2015-03-25 16:37:15 +0100

    start a new swarm container

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
-FROM golang:1.3
+FROM gliderlabs/alpine
+
+MAINTAINER SequenceIQ <info@sequenceiq.com>
+
+RUN apk update && apk add go && rm -rf /var/cache/apk/*
+
+ENV GOROOT /usr/lib/go
+ENV GOPATH /go
+ENV PATH $PATH:$GOROOT/bin:$GOPATH/bin
 
 COPY . /go/src/github.com/sequenceiq/swarm-bootstrap
 WORKDIR /go/src/github.com/sequenceiq/swarm-bootstrap
 
 ENV GOPATH /go/src/github.com/sequenceiq/swarm-bootstrap/Godeps/_workspace:$GOPATH
-RUN go install -v -a
+RUN CGO_ENABLED=0 go install -v -a
 
 ENTRYPOINT ["swarm-bootstrap"]
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/fsouza/go-dockerclient"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -16,37 +17,54 @@ func main() {
 
 	var consulServerCount int
 	flag.IntVar(&consulServerCount, "consulServers", 3, "the number of Consul servers")
-
 	flag.Parse()
+	var nodes string = strings.Join(flag.Args(), ",")
 
 	log.Println("Consul servers:", consulServerCount)
+	log.Println("Nodes:", nodes)
 
-	log.Println("Nodes:", flag.Args())
+	// data := make([]float32, 4)
 
-	data := make([]float32, 4)
-
-	sem := make(chan empty, len(data))
-	for i, _ := range data {
-		go func(i int) {
-			data[i] += 5
-			time.Sleep(1000 * time.Millisecond)
-			log.Println(i, data[i])
-			sem <- empty{}
-		}(i)
-	}
-	for i := 0; i < len(data); i++ {
-		<-sem
-	}
+	// sem := make(chan empty, len(data))
+	// for i, _ := range data {
+	// 	go func(i int) {
+	// 		data[i] += 5
+	// 		time.Sleep(1000 * time.Millisecond)
+	// 		log.Println(i, data[i])
+	// 		sem <- empty{}
+	// 	}(i)
+	// }
+	// for i := 0; i < len(data); i++ {
+	// 	<-sem
+	// }
 
 	endpoint := "unix:///var/run/docker.sock"
 	client, _ := docker.NewClient(endpoint)
 
-	imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
+	config := docker.Config{
+		Image: "swarm",
+		Cmd:   []string{"manage", "-H", "tcp://0.0.0.0:3376", nodes},
+	}
+	container, _ := client.CreateContainer(docker.CreateContainerOptions{Name: "tmp-swarm-mgr", Config: &config})
+	client.StartContainer(container.ID, &docker.HostConfig{})
 
+	container, _ = client.InspectContainer(container.ID)
+	log.Println("Started temporary Swarm manager.", container.ID, container.NetworkSettings.IPAddress)
+
+	log.Println("http://" + container.NetworkSettings.IPAddress + ":3376")
+	tmpSwarmClient, _ := docker.NewClient("http://" + container.NetworkSettings.IPAddress + ":3376")
+
+	log.Println("Wait for swarm manager.")
+	time.Sleep(3000 * time.Millisecond)
+
+	info, _ := tmpSwarmClient.Info()
+
+	imgs, _ := tmpSwarmClient.ListImages(docker.ListImagesOptions{All: false})
 	for _, img := range imgs {
 		fmt.Println("ID: ", img.ID)
 		fmt.Println("RepoTags: ", img.RepoTags)
 	}
 
-	log.Println("siker")
+	log.Println(info)
+
 }

2783cb92f9fdc26b9b9ca3b335c154cbb3ef57ba
Marton Sereg <msereg@hortonworks.com>
2015-05-01 12:46:54 +0200

    CLOUD-576 added wait switch to wait for docker daemons

--- bootstrap.go
+++ bootstrap.go
@@ -15,6 +15,7 @@ import (
 const (
 	MaxGetLeaderAttempts            = 12
 	SecondsBetweenGetLeaderAttempts = 5
+	SecondsBetweenDockerPings       = 5
 )
 
 func bootstrap(c *cli.Context) {
@@ -29,6 +30,7 @@ func bootstrap(c *cli.Context) {
 
 	nodesAsString := c.Args()[0]
 	consulServers := strings.Split(c.String(flConsulServers.Name), ",")
+	wait := c.Int(flWait.Name)
 
 	nodes, err := validateNodeUris(nodesAsString)
 	if err != nil {
@@ -56,8 +58,12 @@ func bootstrap(c *cli.Context) {
 		log.Warnf("[bootstrap] %v consul servers configured. Recommended is 3 or 5.", len(consulServers))
 	}
 
-	log.Infof("[bootstrap] Started Swarm bootstrapping with parameters. Consul servers: %v, Nodes: %s", len(consulServers), nodesAsString)
+	log.Infof("[bootstrap] Started Swarm bootstrapping with parameters. Consul servers: %v, Nodes: %s, Wait: %v", len(consulServers), nodesAsString, wait)
 
+	if wait > 0 {
+		nodes = waitForDockerDaemons(wait, nodes)
+		log.Infof("[bootstrap] Continuing with %v nodes: %s", len(nodes), nodes)
+	}
 	bootstrapNewNodes(nodesAsString, consulServers, nodes, true)
 
 	log.Info("[bootstrap] Finished Swarm bootstrapping.")
@@ -75,6 +81,7 @@ func add(c *cli.Context) {
 	}
 
 	nodesAsString := c.Args()[0]
+	wait := c.Int(flWait.Name)
 
 	consulJoin := c.String(flConsulJoin.Name)
 	if strings.HasPrefix(consulJoin, "consul://") {
@@ -95,11 +102,58 @@ func add(c *cli.Context) {
 	}
 	// TODO: check if consul-servers are new nodes or not, check if other nodes are not in the cluster already
 
-	log.Infof("[bootstrap] Started Swarm add with parameters. Consul servers: %v, Nodes: %s", len(peers), nodesAsString)
+	log.Infof("[bootstrap] Started Swarm add with parameters. Consul servers: %v, Nodes: %s, Wait: %v", len(peers), nodesAsString, wait)
 
+	if wait > 0 {
+		nodes = waitForDockerDaemons(wait, nodes)
+		log.Infof("[bootstrap] Continuing with %v nodes: %s", len(nodes), nodes)
+	}
 	bootstrapNewNodes(nodesAsString, peers, nodes, false)
 
 	log.Info("[bootstrap] Finished adding new nodes to the Consul-Swarm cluster.")
+}
+
+func waitForDockerDaemons(wait int, nodes []string) []string {
+	var maxDockerPingAttemps int = wait/SecondsBetweenDockerPings + 1
+	log.Infof("[bootstrap] Trying to reach docker daemons in %v attempts.", maxDockerPingAttemps)
+	var nodesFound []string
+	for i := 0; ; i++ {
+		if i >= maxDockerPingAttemps {
+			log.Infof("[bootstrap] Max attempts were reached, %v docker daemons are available.", len(nodesFound))
+			return nodesFound
+		}
+		if len(nodesFound) == len(nodes) {
+			log.Infof("[bootstrap] All docker daemons are available.")
+			return nodesFound
+		}
+		time.Sleep(SecondsBetweenDockerPings * time.Second)
+		log.Debugf("[bootstrap] Ping docker daemons, attempt %v", i+1)
+		var wg sync.WaitGroup
+		for _, node := range nodes {
+			wg.Add(1)
+			go func(node string) {
+				defer wg.Done()
+				time.Sleep(1 * time.Second)
+				if contains(nodesFound, node) {
+					log.Debugf("[bootstrap] Docker daemon on node %s has already responded, not pinging it again.", node)
+					return
+				}
+				client, err := docker.NewDockerClientTimeout("http://"+node, nil, time.Duration(5*time.Second))
+				if err != nil {
+					log.Debugf("[bootstrap] Couldn't create client for docker daemon (%s): %s", node, err)
+					return
+				}
+				if _, err := client.Info(); err != nil {
+					log.Debugf("[bootstrap] Failed to retrieve info from docker daemon (%s): %s", node, err)
+				} else {
+					log.Debugf("[bootstrap] Docker daemon responded on node %s.", node)
+					nodesFound = append(nodesFound, node)
+				}
+			}(node)
+		}
+		wg.Wait()
+		log.Infof("[bootstrap] Attempt %v: %v docker daemons are available.", i+1, len(nodesFound))
+	}
 }
 
 func bootstrapNewNodes(nodesAsString string, consulServers []string, nodes []string, startSwarmManager bool) {
@@ -119,7 +173,7 @@ func bootstrapNewNodes(nodesAsString string, consulServers []string, nodes []str
 		log.Fatalf("[bootstrap] Failed to inspect temporary Swarm manager container: %s", err)
 	}
 	log.Debug("[bootstrap] Wait 3 seconds for swarm manager.")
-	time.Sleep(3000 * time.Millisecond)
+	time.Sleep(3 * time.Second)
 	log.Debug("[bootstrap] Creating docker client for temporary Swarm Manager.")
 	tmpSwarmClient, err := docker.NewDockerClient("http://"+tmpSwarmManagerContainer.NetworkSettings.IpAddress+":3376", nil)
 	if err != nil {
@@ -226,4 +280,13 @@ func getConsulPeers(consulAddress string) ([]string, error) {
 		log.Infof("[bootstrap] Consul peers found: %s", peers)
 		return peers, nil
 	}
+}
+
+func contains(stringSlice []string, element string) bool {
+	for _, s := range stringSlice {
+		if s == element {
+			return true
+		}
+	}
+	return false
 }
